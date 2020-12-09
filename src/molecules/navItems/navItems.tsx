b5fb1277a590e151d37a8ae9fc56e571b662a90c
Marcin Niemyjski <marcin.niemyjski@nx-labs.com>
2020-12-09 21:19:35 +0100

    nav items molecule component for navbar

--- src/molecules/navItems/navItems.tsx
+++ src/molecules/navItems/navItems.tsx
@@ -1,1 +1,32 @@
 import React from 'react';
+import styled from 'styled-components'
+import Link from 'atoms/link/link'
+
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+interface InavItem {
+    text: string;
+    url: string;
+}
+
+interface InavItems extends InavItem {
+    items: Array<InavItem>
+}
+
+
+const NavItems = ({items}: InavItems) => {
+    return(
+        <Wrapper>
+            {items.map((item, key) =>(
+                <Link key={key} text={item.text} url={item.url} />
+            ))}
+        </Wrapper>
+    )
+}
+
+
+export default NavItems
